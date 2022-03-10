5241de0dbf9c61e627eaa83d6f9fecdcef8b9e35
Alberto <albertopimentel04@gmail.com>
2022-03-10 15:51:41 -0600

    Menu completo y desarrollo del banner

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+const moreOptions = document.querySelector("#bmore");
+const moreMenu = document.querySelector(".more .menu");
+const bShowMobileLinks = document.querySelector("#bmenu");
+const mobileMenu = document.querySelector(".links");
+
+bShowMobileLinks.addEventListener("click", (e) => {
+  e.preventDefault();
+  mobileMenu.classList.toggle("show");
+});
+
+moreOptions.addEventListener("click", (e) => {
+  e.preventDefault();
+  moreMenu.classList.toggle("show");
+});
+
+
